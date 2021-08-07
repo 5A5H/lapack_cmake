4e4af7165bfa0b664145bb389fa540d55ce6a879
Sascha Maassen <sash@MacBook-Pro.fritz.box>
2021-08-07 12:22:05 +0200

    initialized repo

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# LAPACK_CMake
+An example project to use a LAPACK solver in C++.
+CMake is used to cross platform use the LAPACK libraries instlled on the target system.
---
+++ lapack.cpp
@@ -0,0 +1,32 @@
+// include c++ headder
+#include <memory>
+#include <iostream>
+#include <vector>
+
+extern void dgesv(int* dim1, int* dim2, double* a, int* lda, int* ipiv, double* b, int* ldb, int* info);
+
+int main()
+{
+    std::cout << "Lapack call!" << std::endl;
+    int dim = 2;    
+    int nrhs = 1;
+    int lda = dim;
+    int ldb = dim;
+    int info;
+    int ipiv[dim];
+
+    std::vector<double> a, b;
+
+    a.push_back(1);
+    a.push_back(1);
+    a.push_back(1);
+    a.push_back(-1);
+
+    b.push_back(2);
+    b.push_back(0);
+
+
+    dgesv(&dim, &nrhs,& a[0], &lda, ipiv, & b[0], &ldb, &info);
+
+    return 0;
+}
